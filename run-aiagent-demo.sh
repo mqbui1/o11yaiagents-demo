871ec5a87cc978e33977f534d0f1e03548265329
Marc Bui <mqbui1@gmail.com>
2026-01-28 10:38:50 -0800

    Update run-aiagent-demo.sh

--- run-aiagent-demo.sh
+++ run-aiagent-demo.sh
@@ -13,6 +13,8 @@ pe "helm repo add splunk-otel-collector-chart https://signalfx.github.io/splunk-
 
 pe "helm install splunk-otel-collector --set="splunkObservability.accessToken=VZiHqp4S_JFh0i-10QH2VQ,clusterName=troubleshooting-agent-demo,splunkObservability.realm=us1,gateway.enabled=false,splunkPlatform.endpoint=https://http-inputs-o11y-workshop-us1.splunkcloud.com:443/services/collector/event,splunkPlatform.token=ee8119b5-0fd0-40e6-89c3-c843fabbdf7a,environment=troubleshooting-agent-demo" splunk-otel-collector-chart/splunk-otel-collector"
 
+pe "kubectl get pods"
+
 pe "kubectl get ns k1 >/dev/null 2>&1 || kubectl create ns k1"
 
 pe "kubectl -n k1 apply -f ipbo-busybox.yaml"
